289d85cf3a1b8ec65d3b5d918b42b5a2518305c8
Shushant Tiwari <shushant.tiwari.cs28@iilm.edu>
2026-08-19 23:07:22 +0530

    Add files via upload

---
+++ Input.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Input{
+    public static void main(String[] args){
+        /*Scanner sc = new Scanner(System.in);
+        int Input = sc.nextInt();
+        System.out.println(Input);*/
+        // Scanner sc =new Scanner(System.in);
+        // int input= sc.nextInt();
+        // System.out.println(input);
+        // Scanner sc=new Scanner(System.java);
+        // double Input = sc.nextDouble();
+        // System.out.println(Input); 
+        // Scanner sc=new Scanner(System.in);
+        // float input=sc.nextFloat();
+        // System.out.println(input);
+        // Scanner sc =new Scanner(System.in);
+        // boolean arr=sc.nextBoolean();
+        // System.out.println(arr);
+        // Scanner scn=new Scanner(System.in);
+        // long num=scn.nextLong();
+        // System.out.println(num);
+        // Scanner scn=new Scanner(System.in);
+        // short num=scn.nextShort();
+        // System.out.println(num);
+        // Scanner sc=new Scanner(System.in);
+        // String chr=sc.next();
+        // System.out.println(chr);
+        Scanner sc =new Scanner(System.in);
+        String chr=sc.nextLine();
+        System.out.println(chr);
+        sc.close();
+        
+
+    } 
+    
+}
