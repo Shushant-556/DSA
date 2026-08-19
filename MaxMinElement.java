f744d2c935d75f0439f1d56047bc48a61f2e4e2a
Shushant Tiwari <shushant.tiwari.cs28@iilm.edu>
2026-08-19 23:09:04 +0530

    Add files via upload

---
+++ MaxMinElement.java
@@ -0,0 +1,21 @@
+public class MaxMinElement {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,6};
+        int max=arr[0];
+        int min=arr[0];
+        for(int i=1;i<=arr.length-1;i++){
+            if( max<arr[i]){
+                max=arr[i];
+
+            }
+            if(min>arr[i]){
+                min=arr[i];
+            }
+
+        }
+        System.out.println(max);
+        System.out.println(min);
+        
+    
+    }
+}
